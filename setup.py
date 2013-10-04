e2f1951a065487ddbc0d77ba18fa8ce95b3e325a
Donald Stufft <donald@stufft.io>
2013-10-04 16:41:43 -0400

    Add a versioning and wheel support

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
+import ed25519
+
 
 setup(
-    name="ed25519",
-    version="1.0",
+    name="ed25519.py",
+    version=ed25519.__version__,
 
     py_modules="ed25519",
 
